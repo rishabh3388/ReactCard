ec502fa4f9cc3a126dd118000a665e03c6718ca3
rishabh3388 <rishabhtiwari3388@gmail.com>
2025-08-08 22:04:59 +0530

    sec commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Card from './components/Card'
+
+
+function App() {
+  const data=["rishabh ","saurabh ","rajat ","hari "]
+  return(
+   <div>
+    <Card/>
+   </div>
+  )
+}
+
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+function Card() {
+  const data = [
+    {
+      image: "image.png",
+      title: "Amazon",
+      description: "Amazon is a cloud computing company.",
+    },
+    {
+      image: "img2.png",
+      title: "Facebook",
+      description: "Facebook is a social media platform.",
+    },
+    {
+      image: "img3.png",
+      title: "Instagram",
+      description: "Instagram is a photo and video sharing app.",
+    },
+    {
+      image: "img4.png",
+      title: "Twitter",
+      description: "Twitter is a social media platform.",
+    }
+  ];
+  return (
+    <>
+      <div className="w-full h-screen bg-zinc-300 flex justify-center items-center gap-10">
+        {data.map((val,index)=>(
+          <div className="w-52 rounded-md bg-zinc-200  overflow-hidden">
+          <div className="w-full h-32 bg-zinc-400">
+            <img
+              className=" w-full h-full object-cover"
+              src={val.image}
+              alt=""
+            />
+          </div>
+          <div className="w-full px-3 py-4 bg-white">
+            <h2 className="font-semibold">{val.title}</h2>
+            <p className="text-sm mt-3">
+              {val.description}
+            </p>
+          </div>
+        </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Card;
